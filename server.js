3c0c518222af6159e3bd8c7a27818bd0ee2819d1
DAmini <amini.daniel@gmail.com>
2018-10-21 20:18:21 -0700

    Add html page content, html routes, css & friends.js date

--- server.js
+++ server.js
@@ -8,15 +8,16 @@ const app = express()
 var PORT = process.env.PORT || 3000
 
 // Add Public Library
-app.use(express.static(path.join(__dirname, './app/public')))
+app.use(express.static(path.join(__dirname, './app/pubilc')))
 
 // Add Middleware
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(bodyparser.json())
 
 // Add Routes
-require(path.join(__dirname, './app/routing/apiRoutes'))(app)
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app)
+require(path.join(__dirname, './app/routing/apiRoutes'))
+require(path.join(__dirname, './app/routing/htmlRoutes'))
 
 // Start Listening On PORT
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
